0aec43de0e148c2d0d0a122d6a2ae1da2316a08a
Senthil ACS <senthil.acs@gmail.com>
2026-09-16 17:23:20 +0800

    [UI] Wire React root and authentication provider

---
+++ modern-ui/src/main.tsx
@@ -0,0 +1,10 @@
+import { createRoot } from 'react-dom/client'
+import { AuthProvider } from './auth/AuthProvider'
+import App from './App'
+import './index.css'
+
+createRoot(document.getElementById('root')!).render(
+  <AuthProvider>
+    <App />
+  </AuthProvider>,
+)
