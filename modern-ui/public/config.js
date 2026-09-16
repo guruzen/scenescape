4486d493132a2797a435d38b121f399fc2f6c1da
Senthil ACS <senthil.acs@gmail.com>
2026-09-16 17:21:29 +0800

    [UI] Add runtime configuration injection

---
+++ modern-ui/public/config.js
@@ -0,0 +1,8 @@
+window.__SCENESCAPE_CONFIG__ = {
+  apiBaseUrl: '',
+  keycloakUrl: 'http://localhost:8088/auth',
+  keycloakRealm: 'scenescape',
+  keycloakClientId: 'scenescape-ui',
+  legacyBaseUrl: '/legacy/',
+  appTitle: 'SceneScape',
+}
