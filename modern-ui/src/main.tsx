ce3d24a73133e25d97f3d1e662033fa192ad9b1e
Senthil ACS <senthil.acs@gmail.com>
2026-09-16 23:17:00 +0800

    Load SceneScape theme variants

--- modern-ui/src/main.tsx
+++ modern-ui/src/main.tsx
@@ -2,6 +2,7 @@ import { createRoot } from 'react-dom/client'
 import { AuthProvider } from './auth/AuthProvider'
 import App from './App'
 import './index.css'
+import './themes.css'
 
 createRoot(document.getElementById('root')!).render(
   <AuthProvider>
